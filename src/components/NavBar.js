8361f8403a4f14a605130c15d976aa317ac55d26
Carla Allende <allendecarlasofia@gmail.com>
2021-05-13 14:25:32 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar () {
+    return (
+        <h1>Puro Ímpetu</h1>
+        <nav class="nav-menu mobile-menu">
+          <ul>
+            <li><a href="./index.html">Inicio</a></li>
+            <li><a href="nosotros.html">Nosotros</a></li>
+            <li class="active"><a href="tienda.html">Tienda</a></li>
+            <li><a href="showrooms.html">Showrooms</a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+          </ul>
+        </nav>
+    );
+  }
+  export default NavBar;
